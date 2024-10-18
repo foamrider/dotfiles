7bdd2dbefe7619db4c29b171331a84a77d3d06a1
Tomas Kirkegaard <tomas.kirkegaard@gmail.com>
2024-10-18 21:33:29 +0200

    Update config for Linux

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -9,7 +9,6 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            require("nvchad.configs.lspconfig").defaults()
             require "configs.lspconfig"
         end,
     },
