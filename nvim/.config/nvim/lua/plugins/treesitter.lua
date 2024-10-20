fb0ec2ff5f0c73d1c6cee86e5966eacc4df2443c
Tomas Kirkegaard <tomas.kirkegaard@gmail.com>
2024-10-20 22:07:38 +0200

    Adds customs to zsh

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {
+            "lua",
+            "vim",
+            "svelte",
+            "javascript",
+            "powershell",
+            "bash",
+            "html",
+            "css",
+            "typescript",
+        },
+    },
+}
