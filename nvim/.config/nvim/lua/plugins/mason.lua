e0b462736885d9d5f8ed68a63f4d8a6ad94f05a6
Tomas Kirkegaard <tomas.kirkegaard@gmail.com>
2025-02-18 15:47:15 +0100

    Adds vuels

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -14,6 +14,7 @@ return {
                 "denols",
                 "eslint",
                 "svelte",
+                "vuels",
                 "jsonls",
                 "html",
                 "cssls",
