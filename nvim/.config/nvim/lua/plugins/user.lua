8e5b89d5211964d11f2f9febe03f7bed68a5fa82
Tomas Kirkegaard <tomas.kirkegaard@gmail.com>
2025-03-13 09:30:53 +0100

    Add inline git blame

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -115,4 +115,12 @@ return {
             }
         end,
     },
+    {
+        "braxtons12/blame_line.nvim",
+        config = function()
+            require("blame_line").setup {
+                show_in_insert = false,
+            }
+        end,
+    },
 }
