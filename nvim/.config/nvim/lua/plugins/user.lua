b8127894367141b24758efa24e4e7b391d4c50a0
Tomas Kirkegaard <tomas.kirkegaard@gmail.com>
2025-03-15 21:23:14 +0100

    Add dadbod-ui

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -29,12 +29,12 @@ return {
                 "██║     ╚██████╔╝██║  ██║██║ ╚═╝ ██║   ██║   ",
                 "╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝   ╚═╝   ",
                 " ",
-                "      ███╗   ██╗██╗   ██╗██╗███╗   ███╗      ",
-                "      ████╗  ██║██║   ██║██║████╗ ████║      ",
-                "      ██╔██╗ ██║██║   ██║██║██╔████╔██║      ",
-                "      ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║      ",
-                "      ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║      ",
-                "      ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝      ",
+                "     ███╗   ██╗██╗   ██╗██╗███╗   ███╗       ",
+                "     ████╗  ██║██║   ██║██║████╗ ████║       ",
+                "     ██╔██╗ ██║██║   ██║██║██╔████╔██║       ",
+                "     ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║       ",
+                "     ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║       ",
+                "     ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝       ",
             }
             return opts
         end,
@@ -124,6 +124,23 @@ return {
             require("blame_line").setup {
                 show_in_insert = false,
             }
+        end,
+    },
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = {
+            { "tpope/vim-dadbod", lazy = true },
+            { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+        },
+        cmd = {
+            "DBUI",
+            "DBUIToggle",
+            "DBUIAddConnection",
+            "DBUIFindBuffer",
+        },
+        init = function()
+            -- Your DBUI configuration
+            vim.g.db_ui_use_nerd_fonts = 1
         end,
     },
 }
