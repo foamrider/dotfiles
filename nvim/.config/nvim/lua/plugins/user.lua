80f8b9ef69d591b41f5c5e114c204c24232af862
Tomas Kirkegaard <tomas.kirkegaard@gmail.com>
2025-03-18 10:59:39 +0100

    Add matchit package

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -88,6 +88,9 @@ return {
         end,
     },
     {
+        "chrisbra/matchit",
+    },
+    {
         "olrtg/nvim-emmet",
         config = function() vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation) end,
     },
