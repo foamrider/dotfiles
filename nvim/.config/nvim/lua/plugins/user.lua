292d5fbf66d509acc96bb002e4a4d8ffa9d6b528
Tomas Kirkegaard <tomas.kirkegaard@gmail.com>
2025-03-18 09:02:05 +0100

    Add trouble package to nvim

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -146,7 +146,7 @@ return {
             -- vim.g.db_ui_disable_mappings_dbui = 1
             -- vim.g.db_ui_disable_mappings_dbout = 1
             -- vim.g.db_ui_disable_mappings_sql = 1
-            vim.g.db_ui_use_postgres_views = 0
+            -- vim.g.db_ui_use_postgres_views = 0
         end,
         keys = {
             { "<leader>Dt", "<cmd>DBUIToggle<cr>", desc = "Toggle UI" },
@@ -177,6 +177,43 @@ return {
                 -- return the new table to be used
                 return opts
             end,
+        },
+    },
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
         },
     },
 }
