86af28ec898e6dbe908b8a971982be30372a3156
Tomas Kirkegaard <tomas.kirkegaard@gmail.com>
2025-08-11 13:58:06 +0200

    fix(nvim): add git blameline delay, cleanup unused plugins, update gpt version

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -197,67 +197,6 @@ return {
         },
     },
     {
-        "jake-stewart/multicursor.nvim",
-        branch = "1.0",
-        config = function()
-            local mc = require "multicursor-nvim"
-            mc.setup()
-
-            local set = vim.keymap.set
-
-            -- Add or skip cursor above/below the main cursor.
-            set({ "n", "x" }, "<m-d>", function() mc.matchAddCursor(1) end, { desc = "Skip cursor (next match)" })
-            -- set({ "n", "x" }, "<up>", function() mc.matchAddCursor(-1) end, { desc = "Add cursor (next match)" })
-            -- set({ "n", "x" }, "<down>", function() mc.matchAddCursor(1) end, { desc = "Skip cursor (next match)" })
-            -- set( { "n", "x" }, "<leader><up>", function() mc.matchSkipCursor(-1) end, { desc = "Add cursor (prev match)" })
-            -- set( { "n", "x" }, "<leader><down>", function() mc.matchSkipCursor(1) end, { desc = "Skip cursor (prev match)" })
-
-            -- Add or skip adding a new cursor by matching word/selection
-            -- set({ "n", "x" }, "<leader>n", function() mc.matchAddCursor(1) end, { desc = "Add cursor (next match)" })
-            -- set({ "n", "x" }, "<leader>s", function() mc.matchSkipCursor(1) end, { desc = "Skip cursor (next match)" })
-            -- set({ "n", "x" }, "<leader>N", function() mc.matchAddCursor(-1) end, { desc = "Add cursor (prev match)" })
-            -- set({ "n", "x" }, "<leader>S", function() mc.matchSkipCursor(-1) end, { desc = "Skip cursor (prev match)" })
-
-            -- Add and remove cursors with control + left click.
-            set("n", "<c-leftmouse>", mc.handleMouse)
-            set("n", "<c-leftdrag>", mc.handleMouseDrag)
-            set("n", "<c-leftrelease>", mc.handleMouseRelease)
-
-            -- Disable and enable cursors.
-            set({ "n", "x" }, "<c-q>", mc.toggleCursor, { desc = "Toggle multicursor mode" })
-
-            -- Mappings defined in a keymap layer only apply when there are
-            -- multiple cursors. This lets you have overlapping mappings.
-            mc.addKeymapLayer(function(layerSet)
-                -- Select a different cursor as the main one.
-                layerSet({ "n", "x" }, "<left>", mc.prevCursor)
-                layerSet({ "n", "x" }, "<right>", mc.nextCursor)
-
-                -- Delete the main cursor.
-                layerSet({ "n", "x" }, "<leader>X", mc.deleteCursor, { desc = "Delete current cursor" })
-
-                -- Enable and clear cursors using escape.
-                layerSet("n", "<esc>", function()
-                    if not mc.cursorsEnabled() then
-                        mc.enableCursors()
-                    else
-                        mc.clearCursors()
-                    end
-                end)
-            end)
-
-            -- Customize how cursors look.
-            local hl = vim.api.nvim_set_hl
-            hl(0, "MultiCursorCursor", { reverse = true })
-            hl(0, "MultiCursorVisual", { link = "Visual" })
-            hl(0, "MultiCursorSign", { link = "SignColumn" })
-            hl(0, "MultiCursorMatchPreview", { link = "Search" })
-            hl(0, "MultiCursorDisabledCursor", { reverse = true })
-            hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-            hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
-        end,
-    },
-    {
         "kylechui/nvim-surround",
         version = "*", -- Use for stability; omit to use `main` branch for the latest features
         event = "VeryLazy",
@@ -273,6 +212,7 @@ return {
             require("blame_line").setup {
                 show_in_insert = false,
                 prefix = "     ",
+                delay = 1500,
             }
         end,
     },
@@ -303,33 +243,6 @@ return {
             { "<leader>Df", "<cmd>DBUIFindBuffer<cr>", desc = "Find Buffer" },
             { "<leader>Dr", "<cmd>DBUIRenameBuffer<cr>", desc = "Rename Buffer" },
             { "<leader>Dq", "<cmd>DBUILastQueryInfo<cr>", desc = "Last Query Info" },
-        },
-    },
-    {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        ---@type Flash.Config
-        opts = {
-            modes = {
-                search = {
-                    enabled = true,
-                },
-                char = {
-                    jump_labels = true,
-                },
-            },
-        },
-        keys = {
-            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            {
-                "R",
-                mode = { "o", "x" },
-                function() require("flash").treesitter_search() end,
-                desc = "Treesitter Search",
-            },
-            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         },
     },
     {
@@ -442,7 +355,7 @@ return {
             provider = "copilot",
             providers = {
                 copilot = {
-                    model = "gpt-4.1",
+                    model = "gpt-5",
                 },
             },
             -- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
